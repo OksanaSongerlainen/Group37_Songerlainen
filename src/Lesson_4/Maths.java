2d66ae6a26c94303e046b5baabc59b2829a03414
OSongerlainen <9779589@inbox.ru>
2025-08-31 23:11:45 +0300

    Added new class Test.

---
+++ src/Lesson_4/Maths.java
@@ -0,0 +1,13 @@
+package Lesson_4;
+
+public class Maths {
+    public static void main(String[] args) {
+        System.out.println(Math.max(12, 5));
+    }
+}
+
+class Maths2 {
+    public static void main(String[] args) {
+        System.out.println(Math.round(12.5));
+    }
+}
