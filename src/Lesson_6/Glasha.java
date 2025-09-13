34d3c157fd3593e68f4066f6e13d96d6db63304d
OSongerlainen <9779589@inbox.ru>
2025-09-14 00:32:18 +0300

    Added new class Test.

---
+++ src/Lesson_6/Glasha.java
@@ -0,0 +1,6 @@
+package Lesson_6;
+
+public class Glasha {
+    int age = 3;
+    String bread;
+    }
---
+++ src/Lesson_6/Test_6.java
@@ -0,0 +1,9 @@
+package Lesson_6;
+
+public class Test_6 {public static void main(String[] args) {
+    Glasha glasha = new Glasha();
+    Glasha bob = new Glasha();
+    bob.age = 34;
+       System.out.println(glasha.age + "+" + bob.age);
+}
+}
