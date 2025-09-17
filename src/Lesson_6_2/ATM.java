f4e3b825181c06a60f4dfd135714644f63f10ae4
OSongerlainen <9779589@inbox.ru>
2025-09-17 23:10:29 +0300

    Added new class Test.

--- src/Lesson_6_2/ATM.java
+++ src/Lesson_6_2/ATM.java
@@ -26,7 +26,7 @@ public class ATM {
         }
     }
 
-    /*public boolean withdraw (int amount) {
+    public boolean withdraw(int amount) {
         if (amount <= 0) {
             System.out.println("Сумма должна быть положительной!");
             return false;
@@ -34,8 +34,23 @@ public class ATM {
         int total = banknote20 * 20 + banknote50 * 50 + banknote100 * 100;
         if (amount > total) {
             System.out.println("Недостаточно средств в банкомате.");
-            return false;*/
+            return false;
         }
+        for (int qtyBanknote100 = Math.min(banknote100, amount / 100); qtyBanknote100 >= 0; qtyBanknote100--) {
+            int balance100 = amount - qtyBanknote100 * 100 ;
+            for (int qtyBanknote50 = Math.min(banknote50, balance100 / 50); qtyBanknote50 >= 0; qtyBanknote50--) {
+                int balance50 = balance100 - qtyBanknote50 * 50;
+                if (balance50 >= 0 && balance50 % 20 == 0) {
 
-
-
+                    int balance20 = balance50 / 20;
+                    if (balance20 <= banknote20) {
+                        banknote100 = banknote100 - qtyBanknote100;
+                        banknote50 = banknote50 - qtyBanknote50;
+                        banknote20 = banknote20 - qtyBanknote20;
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+}
