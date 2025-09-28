47b44493575440b5308fd3e4bb6eaf75e2f520f0
OSongerlainen <9779589@inbox.ru>
2025-09-28 16:37:01 +0300

    Added new class Test.

--- src/Lesson_7/Clinic.java
+++ src/Lesson_7/Clinic.java
@@ -12,6 +12,21 @@ public class Clinic {
     Если план лечения имеет код 2 – назначить дантиста и выполнить метод лечить.
     Если план лечения имеет любой другой код – назначить терапевта и выполнить метод
     лечить.*/
+  public static void main(String[] args) {
+      Therapist therapist = new Therapist();
+      System.out.println("Пациент 1 ");
+      Patient patient1 = new Patient("Оля", 1);
+      therapist.doctorsChoice(patient1);
+      System.out.println("Пациент 2 ");
+      Patient patient2 = new Patient("Саша", 2);
+      therapist.doctorsChoice(patient2);
+      System.out.println("Пациент 3 ");
+      Patient patient3 = new Patient("Катя", 3);
+      therapist.doctorsChoice(patient3);
+      System.out.println("Пациент 4 ");
+      Patient patient4 = new Patient("Алексей", 7);
+      therapist.doctorsChoice(patient4);
 
+  }
 
 }
