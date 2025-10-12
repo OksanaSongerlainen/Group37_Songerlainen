0f74715e9fbef6e3ad3017dfe5b1833790e9f3fe
OSongerlainen <9779589@inbox.ru>
2025-10-12 18:59:48 +0300

    Added new class Test.

---
+++ src/Lesson_9_2/Dog.java
@@ -0,0 +1,12 @@
+package Lesson_9_2;
+
+public class Dog {
+   private Dog(){
+    }
+        public static void voice(){
+        System.out.println("Собака издает звук гав-гав. ");
+    }
+    public static void eat(){
+        System.out.println("Собака ест. ");
+    }
+}
