8d552a25215bcde580d1c9c591f6e98ae07ab49a
OSongerlainen <9779589@inbox.ru>
2025-10-19 23:09:38 +0300

    Added new class Test.

---
+++ src/Lesson_10_1/User.java
@@ -0,0 +1,59 @@
+package Lesson_10_1;
+
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private String email;
+    private int age;
+
+    public User(String username, String email, int age) {
+        this.username = username;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String toString() {
+        return "User{" + "Имя пользователя: " + username + '\''
+                + " email: " + email + '\''
+                + ", возраст: " + age
+                + '}';
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(username, user.username) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email, age);
+    }
+}
+
