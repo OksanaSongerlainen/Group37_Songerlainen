93bbcf25cbe995db560005c2ba61987e5588ab48
OSongerlainen <9779589@inbox.ru>
2025-11-23 22:50:52 +0300

    Added new class Test.

--- src/Lesson_13/Person.java
+++ src/Lesson_13/Person.java
@@ -7,27 +7,26 @@ public class Person {
 
     public Person(String login, String password, String confirmPassword) throws WrongLoginException,
             WrongPasswordException {
-        try {
-            if (login.length() >= 20) {
-                throw new WrongLoginException("Логин должен быть меньше 20 символов!");
-            }
-            if (login.contains(" ")){
-                throw new WrongLoginException("Логин не должен содержать пробелы!");
-            }
-            if (password.length() >= 20) {
-                throw new WrongLoginException("Пароль должен быть меньше 20 символов!");
-            }
-            if (password.contains(" ")){
-                throw new WrongLoginException("Пароль не должен содержать пробелы!");
-            }
-            if (!password.matches(".*\\d.*")) {
-                throw new WrongPasswordException("Пароль должен содержать хотя бы одну цифру!");
-            }
-            if (!password.equals(confirmPassword)) {
-                    throw new WrongPasswordException("Пароли не совпадают!");
-                }
-        } catch (WrongLoginException e) {
-            return false;
-        } return true;
+        if (login.length() >= 20) {
+            throw new WrongLoginException("Логин должен быть меньше 20 символов!");
+        }
+        if (login.contains(" ")) {
+            throw new WrongLoginException("Логин не должен содержать пробелы!");
+        }
+        if (password.length() >= 20) {
+            throw new WrongLoginException("Пароль должен быть меньше 20 символов!");
+        }
+        if (password.contains(" ")) {
+            throw new WrongLoginException("Пароль не должен содержать пробелы!");
+        }
+        if (!password.matches(".*\\d.*")) {
+            throw new WrongPasswordException("Пароль должен содержать хотя бы одну цифру!");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают!");
+        }
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
     }
 }
